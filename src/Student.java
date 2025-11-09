5b94b3018e653a7187ce0bf1bcd395fee1e2b266
StefanHornbachner <stefan.hornbachner@htl-wels.at>
2025-11-09 20:08:38 +0100

    Student Konstruktoren + Getter Setter

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,47 @@
+import java.util.Date;
+
 public class Student {
+
+    //--------------Attribute
+    private String name;
+    private String birthday;
+    private float grade;
+    private String schoolClass;
+
+
+    //--------------Konstruktoren
+
+    public Student(){
+        this("Name", "00.00.0000", 0.0f, "Klasse");
+    }
+
+    public Student(String name, String birthday){
+        this(name, birthday, 0.0f, "Klasse");
+    }
+
+    public Student(String name, String birthday, float grade, String schoolClass){
+
+        setName(name);
+        setBirthday(birthday);
+        setGrade(grade);
+        setSchoolClass(schoolClass);
+    }
+
+    //Getter/Setter
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getBirthday() { return birthday; }
+    public void setBirthday(String birthday) { this.birthday = birthday; }
+
+    public float getGrade() { return grade; }
+    public void setGrade(float grade) { this.grade = grade; }
+
+    public String getSchoolClass() { return schoolClass; }
+    public void setSchoolClass(String schoolClass) { this.schoolClass = schoolClass; }
+
+
+    //Methoden
+
+
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,9 @@
 public class Teacher {
+
+    //------------Attribute
+    private String name;
+    private String birthday;
+    private String subject;
+    private String workYears;
+
 }
