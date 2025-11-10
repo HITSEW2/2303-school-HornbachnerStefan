5947c3085d44f67834179b1a9202171b9d4b6356
StefanHornbachner <stefan.hornbachner@htl-wels.at>
2025-11-10 18:44:20 +0100

    Student fertig

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,3 @@
-import java.util.Date;
 
 public class Student {
 
@@ -12,10 +11,12 @@ public class Student {
     //--------------Konstruktoren
 
     public Student(){
+
         this("Name", "00.00.0000", 0.0f, "Klasse");
     }
 
     public Student(String name, String birthday){
+
         this(name, birthday, 0.0f, "Klasse");
     }
 
@@ -28,25 +29,39 @@ public class Student {
     }
 
     //Getter/Setter
+
+    //Name
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    //Birthday
     public String getBirthday() { return birthday; }
     public void setBirthday(String birthday) { this.birthday = birthday; }
 
+    //Grade
     public float getGrade() { return grade; }
-    public void setGrade(float grade) { this.grade = grade; }
+    public void setGrade(float grade) {
 
+        this.grade = grade;
+        if (grade < 1.0){grade = 1.0f; }
+        if (grade > 5.0){grade = 5.0f; }
+    }
+
+    //Class
     public String getSchoolClass() { return schoolClass; }
     public void setSchoolClass(String schoolClass) { this.schoolClass = schoolClass; }
 
 
-    //Methoden
+    //------------Methoden
     public boolean hasPassed(){
         return grade <= 4.0;
     }
 
+    public void improveGrade(float improve){
+        this.grade -= improve;
 
+        if (grade < 1.0){grade = 1.0f; }
+    }
 
 
     //ToString
@@ -54,7 +69,7 @@ public class Student {
     public String toString(){
         String str;
 
-        str = "Rectangle\n";
+        str = "Schüler\n";
         str += "\t Name: " + name + "\n";
         str += "\t Geburtsdatum: " + birthday + "\n";
         str += "\t Note: " + grade + "\n";
